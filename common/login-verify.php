a85bfc0e433bdd4c7fe75c51c9c5db7f072aeb01
Paolo Scapin <paolo.scapin@archifit.com>
2018-10-01 01:25:11 +0200

    rimosso src factory problemi import

--- common/login-verify.php
+++ common/login-verify.php
@@ -11,7 +11,6 @@ define ( 'JPATH_BASE', realpath ( dirname ( __FILE__ ) . $JOOMLA_PATH ) );
 
 require_once (JPATH_BASE . DS . 'includes' . DS . 'defines.php');
 require_once (JPATH_BASE . DS . 'includes' . DS . 'framework.php');
-require_once (JPATH_BASE . DS . 'libraries/src' . DS . 'factory.php');
 
 // import namespace
 use Joomla\CMS\Factory;
