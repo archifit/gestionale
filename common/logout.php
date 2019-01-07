3ab938d14ae1c282615bae01f8a88b3d29bc68eb
Paolo Scapin <paolo.scapin@archifit.com>
2019-01-07 22:27:06 +0100

    login e logout fix per password cambiata

--- common/logout.php
+++ common/logout.php
@@ -1,5 +1,10 @@
 <?php
+require_once __DIR__ . '/__Environment.php';
+require_once __DIR__ . '/path.php';
+require_once __DIR__ . '/__Log.php';
 require_once __DIR__ . '/__Util.php';
+
+info('utente ' . $__username . ': logged out');
 $session->logout();
 
 $base = 'docente';
